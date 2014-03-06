9093e89155e4fd6386123529bdae071a9b2ebf76
niminjie <niminjiecide@gmail.com>
2014-03-06 13:20:30 +0800

    Initial heap

---
+++ heap.py
@@ -0,0 +1,19 @@
+class Node(object):
+
+    def __init__(self):
+        pass
+
+    def create(self):
+        pass
+
+    def add(self):
+        pass
+
+    def delete(self):
+        pass
+
+def test_heap():
+    pass
+
+if __name__ == '__main__':
+    test_heap()
