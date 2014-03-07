546d6c3cf2a80d8ad3134fcf12392e207f9386c6
niminjie <niminjiecide@gmail.com>
2014-03-07 18:20:01 +0800

    Add some function

--- heap.py
+++ heap.py
@@ -1,19 +1,33 @@
 class Node(object):
 
-    def __init__(self):
-        pass
+    def __init__(self, size):
+        self.size = size
+        self.data = data
 
     def create(self):
         pass
 
-    def add(self):
+    def insert(self):
         pass
 
-    def delete(self):
+    def delete_min(self):
+        pass
+
+    def find_min(self):
+        pass
+
+    def is_empty(self):
+        pass
+    
+    def is_full(self):
+        pass
+
+    def make_empty(self):
         pass
 
 def test_heap():
     pass
 
 if __name__ == '__main__':
+    print 'Hello World'
     test_heap()
