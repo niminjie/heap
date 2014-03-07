d4111e746ee43c459450b7c3258974627a2c5ce5
niminjie <niminjiecide@gmail.com>
2014-03-07 19:41:05 +0800

    Insert and delete max element

--- heap.py
+++ heap.py
@@ -1,33 +1,62 @@
-class Node(object):
+def insert_max(heap, value):
+    if len(heap) <= 0:
+        heap.append(0)
+    heap.append(value)
+    heap_up(heap, len(heap) - 1)
 
-    def __init__(self, size):
-        self.size = size
-        self.data = data
 
-    def create(self):
-        pass
+def delete_max(heap, idx):
+    last = len(heap) - 1
+    heap[idx] = heap[last]
+    heap_down(heap, idx)
+    del heap[last]
 
-    def insert(self):
-        pass
-
-    def delete_min(self):
-        pass
-
-    def find_min(self):
-        pass
-
-    def is_empty(self):
-        pass
+def heap_down(heap, idx):
+    number = len(heap) - 2
     
-    def is_full(self):
-        pass
+    child = -1
+    # Havn't any child
+    if 2 * idx > n:
+        return
 
-    def make_empty(self):
-        pass
+    # Have both left and right child
+    elif 2 * idx < n:
+        left = 2 * idx
+        right = 2 * idx + 1
+        if heap[left] < heap[right]:
+            child += right
+
+    # Only have left child
+    elif 2 * idx == n:
+        child = 2 * idx
+
+    if heap[idx] < heap[child]:
+        swap(heap, child, idx)
+        heap_down(heap, child)
+
+def heap_up(heap, idx):
+    if idx > 1:
+        parent = idx // 2
+        parent_value = heap[parent]
+        idx_value = heap[idx]
+
+        if parent_value < idx_value:
+            swap(heap, parent, idx)
+            heap_up(heap, parent)
+
+def swap(heap, value1, value2):
+    tmp = heap[value1]
+    heap[value1] = heap[value2]
+    heap[value2] = tmp
 
 def test_heap():
-    pass
+    insert_queue = [45, 36, 18, 53, 72, 30, 48, 93 ,15 ,35]
+    heap_value = [0]
+
+    for item in insert_queue:
+        insert_max(heap_value, item)
+
+    print heap_value
 
 if __name__ == '__main__':
-    print 'Hello World'
     test_heap()
